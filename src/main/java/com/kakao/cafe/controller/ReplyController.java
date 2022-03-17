88a365f25c5f6e2db2759c7177d93d6780ec86ab
seyeong <seyoung7555@naver.com>
2022-03-17 15:14:01 +0900

    feat(ReplyController): 댓글 작성을 위한 컨트롤러 매핑 작업

---
+++ src/main/java/com/kakao/cafe/controller/ReplyController.java
@@ -0,0 +1,32 @@
+package com.kakao.cafe.controller;
+
+import com.kakao.cafe.controller.dto.ReplySaveRequestDto;
+import com.kakao.cafe.domain.user.User;
+import com.kakao.cafe.service.ReplyService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+public class ReplyController {
+    private final ReplyService replyService;
+
+    public ReplyController(ReplyService replyService) {
+        this.replyService = replyService;
+    }
+
+    @PostMapping("/articles/{articleId}/reply")
+    public String saveReply(@PathVariable Long articleId, ReplySaveRequestDto dto, HttpSession session, Model model) {
+        User sessionUser = (User) session.getAttribute("sessionUser");
+
+        dto.setWriter(sessionUser.getUserId());
+        dto.setArticleId(articleId);
+
+        replyService.save(dto);
+
+        return "redirect:/articles/" + articleId;
+    }
+}

41370b89d2f2d3c09da678a32993b56307b2c736
seyeong <seyoung7555@naver.com>
2022-03-17 15:13:42 +0900

    feat: 댓글 작성을 위한 도메인 객체 Reply 생성

---
+++ src/main/java/com/kakao/cafe/domain/reply/Reply.java
@@ -0,0 +1,37 @@
+package com.kakao.cafe.domain.reply;
+
+public class Reply {
+    private Long id;
+    private String writer;
+    private String contents;
+    private String createdDate;
+    private Long articleId;
+
+    public Reply(Long id, String writer, String contents, String createdDate, Long articleId) {
+        this.id = id;
+        this.writer = writer;
+        this.contents = contents;
+        this.createdDate = createdDate;
+        this.articleId = articleId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getWriter() {
+        return writer;
+    }
+
+    public String getContents() {
+        return contents;
+    }
+
+    public String getCreatedDate() {
+        return createdDate;
+    }
+
+    public Long getArticleId() {
+        return articleId;
+    }
+}

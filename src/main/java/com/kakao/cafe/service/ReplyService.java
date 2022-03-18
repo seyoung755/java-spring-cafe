21acdccfe9529f75daaa0ccacaabec6456b78139
seyeong <seyoung7555@naver.com>
2022-03-18 11:13:28 +0900

    feat(ReplyService): 게시글에 작성된 댓글 목록 불러오기 기능 추가
    - articleId로 조회하면 댓글 리스트가 출력되도록 하였습니다.

--- src/main/java/com/kakao/cafe/service/ReplyService.java
+++ src/main/java/com/kakao/cafe/service/ReplyService.java
@@ -1,8 +1,11 @@
 package com.kakao.cafe.service;
 
 import com.kakao.cafe.controller.dto.ReplySaveRequestDto;
+import com.kakao.cafe.domain.reply.Reply;
 import com.kakao.cafe.domain.reply.ReplyRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ReplyService {
@@ -14,5 +17,9 @@ public class ReplyService {
 
     public void save(ReplySaveRequestDto dto) {
         replyRepository.save(dto.toEntity());
+    }
+
+    public List<Reply> findReplyByArticleId(Long id) {
+        return replyRepository.findReplyByArticleId(id);
     }
 }

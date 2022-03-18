813fceab8fd5f06c08075bfdd64fd59d76ab5f6f
seyeong <seyoung7555@naver.com>
2022-03-18 11:43:53 +0900

    feat(ArticleController): 게시글 상세보기 접속 시 댓글 출력을 위해 컨트롤러 확장
    - ReplyService 객체를 주입받아 replyList를 불러오는 기능을 추가하였습니다.

--- src/main/java/com/kakao/cafe/controller/ArticleController.java
+++ src/main/java/com/kakao/cafe/controller/ArticleController.java
@@ -3,21 +3,26 @@ package com.kakao.cafe.controller;
 import com.kakao.cafe.controller.dto.ArticleSaveRequestDto;
 import com.kakao.cafe.controller.dto.ArticleUpdateRequestDto;
 import com.kakao.cafe.domain.article.Article;
+import com.kakao.cafe.domain.reply.Reply;
 import com.kakao.cafe.domain.user.User;
 import com.kakao.cafe.service.ArticleService;
+import com.kakao.cafe.service.ReplyService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
+import java.util.List;
 
 @Controller
 public class ArticleController {
 
     private final ArticleService articleService;
+    private final ReplyService replyService;
 
-    public ArticleController(ArticleService articleService) {
+    public ArticleController(ArticleService articleService, ReplyService replyService) {
         this.articleService = articleService;
+        this.replyService = replyService;
     }
 
     @GetMapping("/qna")
@@ -35,7 +40,10 @@ public class ArticleController {
     @GetMapping("/articles/{id}")
     public String articleDetail(@PathVariable Long id, Model model) {
         Article article = articleService.findById(id);
+        List<Reply> replyList = replyService.findReplyByArticleId(article.getId());
         model.addAttribute("article", article);
+        model.addAttribute("reply", replyList);
+        model.addAttribute("count", replyList.size());
         return "qna/show";
     }
 

aa4fb6af57f9ca964b931e0482a9c15630db5801
seyeong <seyoung7555@naver.com>
2022-03-18 11:15:10 +0900

    feat(ReplyJdbcTemplateRepository): article_id 값으로 db로부터 댓글 조회하는 기능 구현
    - SELECT문을 통해 reply table에서 찾을 article_id 값을 갖는 댓글 목록을 불러옵니다.

--- src/main/java/com/kakao/cafe/domain/reply/ReplyJdbcTemplateRepository.java
+++ src/main/java/com/kakao/cafe/domain/reply/ReplyJdbcTemplateRepository.java
@@ -1,10 +1,12 @@
 package com.kakao.cafe.domain.reply;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
 import javax.sql.DataSource;
+import java.util.List;
 
 public class ReplyJdbcTemplateRepository implements ReplyRepository {
 
@@ -26,5 +28,23 @@ public class ReplyJdbcTemplateRepository implements ReplyRepository {
         String query = "INSERT INTO reply (writer, contents, article_id) VALUES (:writer, :contents, :article_id)";
 
         namedParameterJdbcTemplate.update(query, params);
+    }
+
+    @Override
+    public List<Reply> findReplyByArticleId(Long articleId) {
+        String query = "SELECT * FROM reply WHERE article_id = " + articleId;
+        return jdbcTemplate.query(query, replyRowMapper());
+    }
+
+
+    private RowMapper<Reply> replyRowMapper() {
+        return (rs, rowNum) -> {
+            long id = rs.getLong("id");
+            String writer = rs.getString("writer");
+            String contents = rs.getString("contents");
+            String createdDate = rs.getTimestamp("created_date").toString();
+            Long articleId = rs.getLong("article_id");
+            return new Reply(id, writer, contents, createdDate, articleId);
+        };
     }
 }
